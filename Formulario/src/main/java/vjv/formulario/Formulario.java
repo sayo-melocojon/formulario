a8c25e5007cbab7169a43cf659a5c1deb4b7c9d0
sayo-melocojon <145472048+sayo-melocojon@users.noreply.github.com>
2023-09-27 08:11:02 -0600

    Add files via upload

---
+++ Formulario/src/main/java/vjv/formulario/Formulario.java
@@ -0,0 +1,13 @@
+package vjv.formulario;
+import javax.swing.*;
+public class Formulario extends JFrame{
+    public Formulario(){
+        setLayout (null);
+    }
+    public static void main(String[] args) {
+        Formulario formulario1=new Formulario();
+        formulario1.setBounds(10, 20, 400, 300);
+        formulario1.setVisible(true);
+        formulario1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
